18bc649235003a2b2b0d18402d49e3ebeddf13e2
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-09-27 02:30:18 +0530

    updates

--- connect.php
+++ connect.php
@@ -7,6 +7,8 @@ session_start();
 $username = "";
 $email = "";
 
+$errors = array();
+
 // connect to db
 
 $db = mysqli_connect('localhost', 'root','','userlogin') or die("could not connect to database");
@@ -17,3 +19,6 @@ $username = mysqli_real_escape_string($db, $_POST['username']);
 $email = mysqli_real_escape_string($db, $_POST['email']);
 $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
 $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+// form validation
+if(empty($username)) {array_push($errors,"Username is required")}
