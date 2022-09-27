affe8e3981bb4a20c22eb4640d0728fbd1592931
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-09-27 13:18:30 +0530

    connect

--- connect.php
+++ connect.php
@@ -21,10 +21,10 @@ $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
 $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
 // form validation
-if(empty($username)) {array_push($errors,"Username is required")};
-if(empty($email)) {array_push($errors,"Email is required")};
-if(empty($password_1)) {array_push($errors,"Password is required")};
-if(empty($password_1 != $password_2)) {array_push($errors,"Password do not match")};
+if(empty($username)) {array_push($errors,"Username is required");
+if(empty($email)) {array_push($errors,"Email is required");}
+if(empty($password_1)) {array_push($errors,"Password is required");}
+if(empty($password_1 != $password_2)) {array_push($errors,"Password do not match");}
 
 // check db for existing user with same username
 
@@ -36,5 +36,17 @@ $user = mysqli_fetch_assoc($results);
 if($user) {
     if($user['username'] === $username){array_push($errors, "Username already exists");}
     if($user['email'] === $email){array_push($errors, "This email id already has a registered username");}
-    
+
 }
+
+// Register th userif no error
+
+if(count($errors) == 0) 
+    $password = md5($password_1); //this will encrypt the password
+    $query = "INSERT INTO user(username, email, password) VALUES ('$username', '$email', '$password')";
+
+    mysqli_query($db,$query);
+    $_SESSION['username'] = $username;
+    $_SESSION['success'] = "You are now logged in";
+
+    header('location: index.php');
