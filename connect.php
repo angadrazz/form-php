bf22d929a80ce94941d2f2802e5ae1fd18267407
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-09-27 12:29:33 +0530

    connect

--- connect.php
+++ connect.php
@@ -21,4 +21,20 @@ $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
 $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
 // form validation
-if(empty($username)) {array_push($errors,"Username is required")}
+if(empty($username)) {array_push($errors,"Username is required")};
+if(empty($email)) {array_push($errors,"Email is required")};
+if(empty($password_1)) {array_push($errors,"Password is required")};
+if(empty($password_1 != $password_2)) {array_push($errors,"Password do not match")};
+
+// check db for existing user with same username
+
+$user_check_query = "SELECT * FROM user WHERE username = '$username' or email = 'email' LIMIT 1";
+
+$results = mysqli_query($db, $user_check_query);
+$user = mysqli_fetch_assoc($results);
+
+if($user) {
+    if($user['username'] === $username){array_push($errors, "Username already exists");}
+    if($user['email'] === $email){array_push($errors, "This email id already has a registered username");}
+    
+}
